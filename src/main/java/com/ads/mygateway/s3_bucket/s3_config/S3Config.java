37e67945193d65ac0385d580dffa181b2ba4d075
Adabala-Dinesh <dadabala@neoterictechnologiesinc.com>
2025-11-13 20:12:55 +0530

    added aws keys in render

--- src/main/java/com/ads/mygateway/s3_bucket/s3_config/S3Config.java
+++ src/main/java/com/ads/mygateway/s3_bucket/s3_config/S3Config.java
@@ -1,44 +1,38 @@
-//package com.ads.mygateway.s3_bucket.s3_config;
-//
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-//import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-//import software.amazon.awssdk.regions.Region;
-//import software.amazon.awssdk.services.s3.S3Client;
-//
-//@Configuration
-//public class S3Config {
-//
-//    private final String accessKey;
-//    private final String secretKey;
-//    private final String region;
-//    private final String masterKey;
-//    private final String salt;
-//
-//    public S3Config(
-//            @Value("${aws.accessKey}") String accessKey,
-//            @Value("${aws.secretKey}") String secretKey,
-//            @Value("${aws.region}") String region,
-//            @Value("${avoota.crypto.mk}") String masterKey,
-//            @Value("${avoota.crypto.slt}") String salt
-//    ) {
-//        this.accessKey = PropertyEDCrypt.decrypt(accessKey, masterKey, salt);
-//        this.secretKey = PropertyEDCrypt.decrypt(secretKey, masterKey, salt);
-//        this.region = region;
-//        this.masterKey = masterKey;
-//        this.salt = salt;
-//    }
-//
-//    @Bean
-//    public S3Client s3Client() {
-//        return S3Client.builder()
-//                .region(Region.of(region))
-//                .credentialsProvider(
-//                        StaticCredentialsProvider.create(
-//                                AwsBasicCredentials.create(accessKey, secretKey)))
-//                .build();
-//    }
-//}
+package com.ads.mygateway.s3_bucket.s3_config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+
+    private final String accessKey;
+    private final String secretKey;
+    private final String region;
+
+    public S3Config(
+            @Value("${aws.accessKey}") String accessKey,
+            @Value("${aws.secretKey}") String secretKey,
+            @Value("${aws.region}") String region
+    ) {
+        this.accessKey = accessKey;
+        this.secretKey = secretKey;
+        this.region = region;
+    }
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(accessKey, secretKey)))
+                .build();
+    }
+}
